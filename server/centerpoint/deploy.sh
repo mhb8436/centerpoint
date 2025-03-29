5457bef5b5f8590d37941b0d67f04c8a597c53d6
mhb8436 <mhb8436@gmail.com>
2025-03-30 08:37:30 +0900

    change keyfile

--- server/centerpoint/deploy.sh
+++
@@ -1,5 +0,0 @@
-./mvnw clean package
-scp -i ~/.ssh/mhb8436.pem /Users/ethan.lee781/MyWorks/centerpoint/server/centerpoint/oauth2-server/target/oauth2-server-0.0.1-SNAPSHOT.war ubuntu@api.pizzastudio.app:~/oauth2-server.war
-scp -i ~/.ssh/mhb8436.pem /Users/ethan.lee781/MyWorks/centerpoint/server/centerpoint/api-server/target/api-server-0.0.1-SNAPSHOT.war ubuntu@api.pizzastudio.app:~/api-server.war
-
-
--- server/curl.test.sh
+++ server/curl.test.sh
@@ -1,18 +1,1 @@
-curl  -H 'Content-Type: application/json' -H "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzY29wZSI6WyJtZW1iZXIuaW5mby5wdWJsaWMiXSwiZXhwIjoxNTY2MDM3MDc0LCJhdXRob3JpdGllcyI6WyJST0xFX01ZX0NMSUVOVCJdLCJqdGkiOiI0OWFhNjU5NC05ODg4LTQ2OWUtYWFhMi02YjcyNTQxZWZlMjMiLCJjbGllbnRfaWQiOiJteV9jbGllbnRfaWQifQ.fzjwaElVtCLRJ5_4MHds98jgssS6s5YpX9jgWCnQi-s"  -X POST  -d '[{"lat":"34.781676","lng":"126.822107","name":"point0","type":"car"},{"lat":"35.271984","lng":"128.393152","name":"point1","type":"car"},{"lat":"35.884056","lng":"127.228602","name":"point2","type":"car"},{"lat":"34.894392","lng":"126.772669","name":"point3","type":"car"}]' "http://localhost:8081/api/new/cp"
-
-
-
-
-
-
-curl  -H 'Content-Type: application/json' -H "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzY29wZSI6WyJtZW1iZXIuaW5mby5wdWJsaWMiXSwiZXhwIjoxNTY2MDM3MDc0LCJhdXRob3JpdGllcyI6WyJST0xFX01ZX0NMSUVOVCJdLCJqdGkiOiI0OWFhNjU5NC05ODg4LTQ2OWUtYWFhMi02YjcyNTQxZWZlMjMiLCJjbGllbnRfaWQiOiJteV9jbGllbnRfaWQifQ.fzjwaElVtCLRJ5_4MHds98jgssS6s5YpX9jgWCnQi-s"  -X POST  -d '[{"lat":"37.52463","lng":"126.909104","name":"point0","type":"car"},{"lat":"36.784743","lng":"127.183763","name":"point1","type":"car"},{"lat":"35.810778","lng":"128.546067","name":"point2","type":"car"},{"lat":"37.685649","lng":"128.859178","name":"point3","type":"car"}]' "http://localhost:8081/api/new/cp"
-
-
-[{"lat":"37.52463","lng":"126.909104","name":"point0","type":"car"},{"lat":"36.784743","lng":"127.183763","name":"point1","type":"car"},{"lat":"35.810778","lng":"128.546067","name":"point2","type":"car"},{"lat":"37.685649","lng":"128.859178","name":"point3","type":"car"}]
-
-
-
-curl -d "client_id=my_client_id&client_secret=my_client_secret&grant_type=client_credentials&scope=member.info.public" -X POST "http://my_client_id:my_client_secret@api.pizzastudio.app/oauth2-server/oauth/token"
-
-
-curl  -H 'Content-Type: application/json' -H "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzY29wZSI6WyJtZW1iZXIuaW5mby5wdWJsaWMiXSwiZXhwIjoxNTY2MDQ1NDUwLCJhdXRob3JpdGllcyI6WyJST0xFX01ZX0NMSUVOVCJdLCJqdGkiOiI0MjI4OGIwMC1kZjE0LTQxODctODllZC0wZmYzMDcxMTdkNjAiLCJjbGllbnRfaWQiOiJteV9jbGllbnRfaWQifQ.AG7klkpTsM5OaDLdmGMACZ2r3hT-wLBr83kO7FOkOiA"  -X POST  -d '[{"lat":"37.52463","lng":"126.909104","name":"point0","type":"car"},{"lat":"36.784743","lng":"127.183763","name":"point1","type":"car"},{"lat":"35.810778","lng":"128.546067","name":"point2","type":"car"},{"lat":"37.685649","lng":"128.859178","name":"point3","type":"car"}]' "http://api.pizzastudio.app/api-server/api/new/cp"
+ 
