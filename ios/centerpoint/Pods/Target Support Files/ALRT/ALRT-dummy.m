7867cf68ef2fe223ad0105bc5290f901e5c79191
mhb8436 <mhb8436@gmail.com>
2025-03-29 18:48:42 +0900

    Pod Delete

--- ios/centerpoint/Pods/Target Support Files/ALRT/ALRT-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_ALRT : NSObject
-@end
-@implementation PodsDummy_ALRT
-@end
--- ios/centerpoint/Pods/Target Support Files/SmartWKWebView/SmartWKWebView-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_SmartWKWebView : NSObject
-@end
-@implementation PodsDummy_SmartWKWebView
-@end
